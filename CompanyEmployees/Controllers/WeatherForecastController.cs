fe56ecea2118c56ead86f55b9e3cbb92260eb4f0
Borche Blazhevski <blazhevski.borche@outlook.com>
2021-03-26 13:37:10 +0100

    Add project

---
+++ CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CompanyEmployees.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private ILoggerManager _logger;
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public WeatherForecastController(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Info message from our controller.");
+            _logger.LogDebug("Debug message from our controller.");
+            _logger.LogWarn("Warn message from our controller.");
+            _logger.LogError("Error message from our controller.");
+
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
