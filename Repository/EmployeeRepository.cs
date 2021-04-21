f3a3c9dd172c86a7a2a7d8d9e6521057a20b539a
Borche Blazhevski <blazhevski.borche@outlook.com>
2021-04-21 20:44:17 +0200

    Sorting

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -3,7 +3,8 @@ using Entities;
 using Entities.Models;
 using Entities.RequestFeatures;
 using Microsoft.EntityFrameworkCore;
-using Repository.Extensions.Repository.Extensions;
+using Repository.Extensions;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,7 +24,7 @@ namespace Repository
             var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
             .FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge)
             .Search(employeeParameters.SearchTerm)
-            .OrderBy(e => e.Name)
+            .Sort(employeeParameters.OrderBy)
             .ToListAsync();
 
             return PagedList<Employee>
