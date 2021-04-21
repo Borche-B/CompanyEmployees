f735982362ee1ddd01fc2f336d209b64b79beb1a
Borche Blazhevski <blazhevski.borche@outlook.com>
2021-04-21 16:21:43 +0200

    searching

---
+++ Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Extensions
+{
+    using Entities.Models;
+    using System;
+    using System.Linq;
+
+    namespace Repository.Extensions
+    {
+        public static class RepositoryEmployeeExtensions
+        {
+            public static IQueryable<Employee> FilterEmployees(this IQueryable<Employee> employees, uint minAge, uint maxAge) =>
+                employees.Where(e => (e.Age >= minAge && e.Age <= maxAge));
+
+            public static IQueryable<Employee> Search(this IQueryable<Employee> employees, string searchTerm)
+            {
+                if (string.IsNullOrWhiteSpace(searchTerm))
+                    return employees;
+
+                var lowerCaseTerm = searchTerm.Trim().ToLower();
+
+                return employees.Where(e => e.Name.ToLower().Contains(lowerCaseTerm));
+            }
+        }
+    }
+
+    public static class RepositoryEmployeeExtensions
+    {
+
+    }
+}
